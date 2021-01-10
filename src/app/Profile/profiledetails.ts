a74136137c2f552a5c1b2e399c09e87ac63d0730
dhanush614 <dhanushkumar614@gmail.com>
2021-01-10 17:34:10 +0530

    first commit

---
+++ src/app/Profile/profiledetails.ts
@@ -0,0 +1,6 @@
+export class ProfileDetails {
+    name: string;
+    price: string;
+    bet: string;
+    profileImage: string;
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { ProfileDetails } from './Profile/profiledetails';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'coda-challenge-game';
+
+export class AppComponent implements OnInit {
+  title: string;
+  betsApiUrl: string;
+  betsList: ProfileDetails[];
+  
+   constructor(private http: HttpClient) {
+   	this.title = 'CodaChallenge'; 
+    this.betsApiUrl = 'https://cors-anywhere.herokuapp.com/s3-ap-southeast-1.amazonaws.com/he-public-data/bets7747a43.json';
+  }
+  
+  ngOnInit(){
+  	this.getItems().subscribe(data => {
+      this.betsList = data;
+      console.log(this.betsList);
+    });
+  }
+  
+  public getItems(): Observable<ProfileDetails[]> {
+    return this.http.get<ProfileDetails[]>(this.betsApiUrl);
+  }
 }
